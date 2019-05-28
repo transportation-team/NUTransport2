633bb71f8764f94ebb5ab1c11cec23ebe37aefba
danielwilensky <danielwilensky2019@u.northwestern.edu>
2019-05-28 18:51:04 -0500

    location stuff

--- LSchedule.2.js
+++ LSchedule.2.js
@@ -11,15 +11,16 @@ function generate_table() {
 
     var myObj = new Object();
     myObj.stop = ["Linden", "Central", "Noyes", "Howard",];
-    myObj.Linden = { "pmSwitch": 4, "times": ["0:20", "04:40", "06:10", "11:30", "01:00", "05:00"] }
-    myObj.Central = { "pmSwitch": 4, "times": ["0:30", "04:50", "06:30", "11:45", "01:10", "05:15"] }
-    myObj.Noyes = { "pmSwitch": 4, "times": ["0:50", "05:00", "06:40", "11:55", "01:20", "05:25"] }
-    myObj.Howard = { "pmSwitch": 3, "times": ["0:55", "05:05", "06:50", "0:05", "01:30", "05:40"] }
+    myObj.Linden = {"times": ["0:20", "04:40", "06:10", "11:30", "01:00", "05:00"] }
+    myObj.Central = {"times": ["0:30", "04:50", "06:30", "11:45", "01:10", "05:15"] }
+    myObj.Noyes = {"times": ["0:50", "05:00", "06:40", "11:55", "01:20", "05:25"] }
+    myObj.Howard = { "times": ["0:55", "05:05", "06:50", "0:05", "01:30", "05:40"] }
     myObj.numTimes = 6;
     var NUMSTOPS = 4;
 
     //Suppose we wanted to arrive at Central by 3:00 pm, from Linden
-    var destination = "Central";
+    var destination = document.getElementById("location").value;
+    console.log(destination);
     var textboxValue = document.getElementById("time12").value;
     //var destination = textboxValue;
     var time = textboxValue;
@@ -32,13 +33,11 @@ function generate_table() {
     var today = new Date();
     var currTime = today.getHours() + ":" + today.getMinutes();
 
+    //finds first time after when input time is
     while (continueLoop == true) {
         currTime = myObj[destination]['times'][i];
         currTime = currTime.split(":")
         var currTimeConverted = parseInt(currTime[0]) * 60 + parseInt(currTime[1]);
-        if (myObj[destination]['pmSwitch'] <= i) {
-            currTimeConverted = currTimeConverted + 12 * 60;
-        }
         if (currTimeConverted < timeConverted) {
             i = i + 1;
         }
@@ -54,7 +53,8 @@ function generate_table() {
     //Now i-1 is the correct entry
     var lastTrip = i;
     var numColumns = 5;
-    if (numColumns > lastTrip) {
+    if (numColumns > lastTrip) 
+    {
         numColumns = lastTrip;
     }
     //    var firstTrip = max(0, lastTrip-5);
